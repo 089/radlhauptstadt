2ad0ca39c528540805a58b244f814d169b0038ed
089 <zell@hm.edu>
2017-02-03 13:39:11 +0100

    trying other routes

--- REST/REST.py
+++ REST/REST.py
@@ -7,22 +7,31 @@ import json
 
 app = Flask(__name__)
 
-is_db_config_loaded = False
+def load_db_config_from_json():
+    pass
 
-@app.route('/radlhauptstadt/api/v0.9/provider/mvg/vehicle')
+is_db_config_loaded = False
+config = load_db_config_from_json()
+
+@app.route('/')
+@app.route('/index')
+#@app.route('/radlhauptstadt/api/v0.9/provider/mvg/vehicle')
 def hello_world():
     return 'Hello World!'
 
 def load_db_config_from_json():
+    global is_db_config_loaded
     config_file_name = ('config.json')
     with open(config_file_name) as config_file:
-        config = json.load(config_file)
+        cfg = json.load(config_file)
     is_db_config_loaded = True
-    return config
+    return cfg
 
 
 def get_db():
-    if is_db_config_loaded == False:
+    global is_db_config_loaded, config
+
+    if not is_db_config_loaded:
         config = load_db_config_from_json()
 
     if not hasattr(g, 'mysql_db'):
