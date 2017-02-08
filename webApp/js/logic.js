9e4a327c9ea81d9c29783452e6757b1aa4ce9fd5
Rudi Loderer <rudi-l.6@gmx.de>
2017-02-08 11:39:38 +0100

    add diagram, return area layers default disabled

--- webApp/js/logic.js
+++ webApp/js/logic.js
@@ -11,14 +11,14 @@ let dbBicycleLayer = new L.LayerGroup();
 DB_BICYCLE_RETURN_AREA = "DB-Rad Rückgabegebiet";
 let dbBicycleReturnAreaLayer = new L.LayerGroup();
 MVV_BICYCLE_RETURN_AREA = "MVV-Rad Rückgabegebiet";
-let mvvBicycleReturnAreaLayer = new L.LayerGroup();
+let mvgBicycleReturnAreaLayer = new L.LayerGroup();
 
 
 let overlayLayerControl = {
 				[MVG_BICYCLE]: mvgBicycleLayer,
                 [DB_BICYCLE]: dbBicycleLayer,
                 [DB_BICYCLE_RETURN_AREA]: dbBicycleReturnAreaLayer,
-                [MVV_BICYCLE_RETURN_AREA]: mvvBicycleReturnAreaLayer
+                [MVV_BICYCLE_RETURN_AREA]: mvgBicycleReturnAreaLayer
 	};
 
 // create icons
@@ -64,11 +64,14 @@ function create(){
 	// Karte erzeugen
 	map = L.map('map', {layers: [baseLayer, mvgBicycleLayer,
 			dbBicycleLayer, dbBicycleReturnAreaLayer,
-			mvvBicycleReturnAreaLayer]})
+			mvgBicycleReturnAreaLayer]})
 		.setView([48.137220, 11.575496], 12);
 
 	// layer control hinzufügen
 	L.control.layers(null, overlayLayerControl).addTo(map);
+
+	map.removeLayer(mvgBicycleReturnAreaLayer)
+	map.removeLayer(dbBicycleReturnAreaLayer);
 }
 
 // Fügt einen Fahrrad-Marker hinzu...
@@ -145,7 +148,7 @@ function addArea(){
     [48.125468, 11.652927],
     [48.087062, 11.617409]
 	]).setStyle({fillColor: '#4562a2', color: '#4562a2'})
-	.addTo(mvvBicycleReturnAreaLayer);
+	.addTo(mvgBicycleReturnAreaLayer);
 }
 
 function generateBicyclePopup(number, type){
